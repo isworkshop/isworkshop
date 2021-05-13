bd3c39af82d1db82e27a6c3a220a3cad86aa4643
isworkshop <office@isworkshop.net>
2021-05-13 15:51:37 +0200

    Poslednja verzija aktivnog sajta

--- dist/en/php/neuspesna.php
+++
@@ -1,6 +0,0 @@
-<div class="container-uspesna">
-  <div class="wrapper">
-      <h3>Došlo je do greške,pokušajte ponovo.</h3>
-      <a href="https://www.isworkshop.net">Povratak na sajt</a>
-  </div>
-</div> 
--- dist/en/php/uspesna.php
+++
@@ -1,8 +0,0 @@
-
-
-<div class="container-uspesna">
-  <div class="wrapper">
-      <h3>Vaša poruka je uspešno poslata.</h3>
-      <a href="https://www.isworkshop.net">Povratak na sajt</a>
-  </div>
-</div> 
