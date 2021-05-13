a2be09b1f2d93eaab3efa87eb5ab06809efe1ce7
isworkshop <office@isworkshop.net>
2021-05-13 15:35:05 +0200

    Sredjivanje i povezivanje svih razbacanih i nepovezanih delova; izbacivanje 'en' foldera i dupliranih fajlova

---
+++ dist/en/php/uspesna.php
@@ -0,0 +1,8 @@
+
+
+<div class="container-uspesna">
+  <div class="wrapper">
+      <h3>Vaša poruka je uspešno poslata.</h3>
+      <a href="https://www.isworkshop.net">Povratak na sajt</a>
+  </div>
+</div> 
